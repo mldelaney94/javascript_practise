488de66169264b23c6165c9c1f8a04b8707bab25
matthew <mldelaney94@gmail.com>
2019-07-31 18:51:56 +1000

    Started javascript practise

---
+++ javascript_learning_projects/messing_with_DOM/scripts.js
@@ -0,0 +1,11 @@
+    function changeColor() {
+        document.getElementById("color_p").style.color = "green";
+    }
+
+    function displayDate () {
+        document.getElementById("date_p").innerText = Date();
+    }
+
+    function changePStyle() {
+        document.getElementById("css_js_p").className = "unique";
+    }
